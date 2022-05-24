6e7fa7ff635221e3a019edbb54f076a81470daa8
amani_usagi <amaniusagi06@gmail.com>
2022-05-24 13:34:07 +0300

    Initial commit

---
+++ src/emailsapp/Email.java
@@ -0,0 +1,34 @@
+package emailsapp;
+
+public class Email {
+	
+	//variable setup
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	
+	//Constructor to receive first and last name
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("EMAIL CREATED: " + this.firstName+ " " + this.lastName);
+	}
+	
+	//Check the departments
+	
+	
+	//Generate a random password
+	
+	
+	//Set the mailbox capacity
+	
+	
+	//Set the alternate email
+	
+	
+	//Change the password
+	
+}
